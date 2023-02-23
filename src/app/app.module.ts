c72f983b0c081882f30ad134d40cbf29328cacdd
Mamushe <tewodroslemma64@gmail.com>
2023-02-23 06:04:44 -0500

    import newly created comp

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { GenTablesComponent } from './gen-tables/gen-tables.component';
+import { LoginComponent } from './landing/login/login.component';
+import { HomeComponent } from './landing/home/home.component';
+import { SideBarComponent } from './landing/side-bar/side-bar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    GenTablesComponent,
+    LoginComponent,
+    HomeComponent,
+    SideBarComponent
   ],
   imports: [
     BrowserModule,
