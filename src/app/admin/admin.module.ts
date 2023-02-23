2fee9a9f9b47db193756b5f5befeaea12b1ecdc4
Mamushe <tewodroslemma64@gmail.com>
2023-02-23 04:58:35 -0500

    admin module for admin role

---
+++ src/app/admin/admin.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { UserControllComponent } from './user-controll/user-controll.component';
+
+
+
+@NgModule({
+  declarations: [
+    UserControllComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class AdminModule { }
