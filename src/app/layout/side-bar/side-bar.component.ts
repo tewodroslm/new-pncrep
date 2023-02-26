bdc52b88ac902990db7cef55757907eb83654768
Mamushe <tewodroslemma64@gmail.com>
2023-02-26 04:38:43 -0500

    side nav check if home hide the others

--- src/app/layout/side-bar/side-bar.component.ts
+++ src/app/layout/side-bar/side-bar.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-side-bar',
@@ -7,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SideBarComponent implements OnInit {
 
-  constructor() { }
+  constructor(public router: Router) { }
 
   showFiller = true;
 
