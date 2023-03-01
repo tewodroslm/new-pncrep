59c9b21ec74ff5ed147fbca536b1f98a89160ebc
Mamushe <tewodroslemma64@gmail.com>
2023-03-01 13:26:26 -0500

    hold authenticated users info

---
+++ src/app/data-share.service.ts
@@ -0,0 +1,30 @@
+import { Injectable } from '@angular/core';
+import { Router } from '@angular/router';
+import { User } from './models/user';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class DataShareService {
+
+  paymentTab = false;
+  adminTab = false;
+  reportTab = false;
+
+  user: User = new User();
+
+  constructor(private router: Router) { }
+
+  public set userInfo(user: any){
+    this.user.username = user.username;
+    this.user.lastname = user.lastname;
+    this.user.role = [...user.role];
+    this.user.email = user.email;
+  }
+
+  
+  getUser(){
+    return this.user;
+  }
+   
+}
