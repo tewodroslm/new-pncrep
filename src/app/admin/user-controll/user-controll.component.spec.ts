53a81eafca4168e9ad6dea2d2b0ec0bd437ed5f2
samuel6tesfay <samuel6tesfay@gmail.com>
2023-06-08 07:02:43 -0400

    unit test for user control component

--- src/app/admin/user-controll/user-controll.component.spec.ts
+++ src/app/admin/user-controll/user-controll.component.spec.ts
@@ -1,14 +1,68 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { UserControllComponent } from './user-controll.component';
+import { Observable } from 'rxjs';
+import { UserService } from '../user-service';
 
 describe('UserControllComponent', () => {
   let component: UserControllComponent;
   let fixture: ComponentFixture<UserControllComponent>;
 
+  const mockService = {
+
+    getBasicUser:()=>{
+       return new Observable((obs)=>{
+        obs.next({
+          response:{
+            body:{
+              id:'1',
+              name:'samuel',
+              lastName:'tesfay',
+              roles:[
+                {
+                  'user': 'tederos',
+                  'role':'USER'
+                }
+              ],
+              email:'samuel@gmail.com',
+            }
+          }
+          
+
+        });
+        obs.complete();
+       })
+    },
+    getManagers(){
+      return new Observable((obs)=>{
+        obs.next({
+          response:{
+            body:{
+              id:'1',
+              name:'samuel',
+              lastName:'tesfay',
+              roles:[
+                {
+                  'user': 'samuel',
+                  'role':'MANAGER'
+                },
+              ],
+              email:'samuel@gmail.com',
+            }
+          }
+            
+        });
+        obs.complete();
+       })
+    }
+  }
+
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ UserControllComponent ]
+      declarations: [ UserControllComponent ],
+      providers:[
+        {provide:UserService,useValue:mockService}
+      ]
     })
     .compileComponents();
   });
@@ -22,4 +76,16 @@ describe('UserControllComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should method tab changed ' , () => {
+      let event = {
+        index:1
+      }
+      component.onTabChanged(event);
+      event.index = 0;
+      component.onTabChanged(event);
+      expect(component.dataSource[0]).toEqual(undefined);
+  })
+
+
 });
